e4ac2285402e37c7de8676a0a1da07e3e489e2a5
alex <alexandre.venet@hotmail.com>
2021-02-19 03:35:06 +0200

    finished task 4_1

--- select.py
+++ select.py
@@ -1,7 +1,9 @@
 from matplotlib import pyplot as plt
 import cv2 as cv
 
-img1 = cv.imread('./fd_jpg/IMG_9130.jpg')
+# img1 = cv.imread('./hg_jpg/IMG_9135.jpg')
+img1 = cv.imread('./hg_jpg/IMG_9138_2.jpg')
+
 img1 = cv.resize(img1, (1368, 912))
 
 plt.imshow(img1)
